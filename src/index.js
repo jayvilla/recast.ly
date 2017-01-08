eb942290d249cd03f427e60470e3780629ac60da
jayvilla <villarina.jeffrey@gmail.com>
2017-01-07 16:41:57 -0800

    Implement minor code cleanup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 // TODO: Render the `App` component to the DOM
 ReactDOM.render(<App searchYouTube={window.searchYouTube} searchYouTubeViews={window.searchYouTubeViews}/>, document.getElementById('app'));
+
+// class Home extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     console.log(props);
+//   }
+
+//   render () {
+//     return (<App searchYouTube={window.searchYouTube} searchYouTubeViews={window.searchYouTubeViews}/>);
+//   }
+// }
+// ReactDOM.render((
+//   <ReactRouter.Router history={ReactRouter.browserHistory}>
+//     <ReactRouter.Route path="/" component={Home} >
+//       <ReactRouter.DefaultRoute handler={Home}/>
+//       <ReactRouter.NotFoundRoute handler={Home}/>
+//     </ReactRouter.Route>
+//   </ReactRouter.Router>)
+// , document.getElementById('app'));
